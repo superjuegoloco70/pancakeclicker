342bece85998e8bdc0944aea3504769c39b29c48
superjuegoloco70 <jonayranz@gmail.com>
2025-01-25 13:06:06 -0300

    Updated Scripts

--- Scripts/Game/GameManager.cs
+++ Scripts/Game/GameManager.cs
@@ -3,30 +3,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] TMP_Text moneyCounter;
-    [SerializeField] GameObject main, story;
+    [SerializeField] TMP_Text moneyCounter, workHarderLvlCostTxt, workHarderMaxLvlTxt, effLvlCostTxt, effMaxLvlTxt;
+    [SerializeField] GameObject main, study;
+    [SerializeField] GameObject story;
 
     void Start()
     {
-        this.moneyCounter.text = "$" + PlayerStats.money.ToString();
-        if(PlayerStats.storyProgress == 0){
-            main.SetActive(false);
-            story.SetActive(true);
-        }
+        moneyCounter.text = "$" + PlayerStats.money;
+        main.SetActive(false);
+        story.SetActive(true);
+        study.SetActive(false);
+        workHarderLvlCostTxt.text = "Coste: $" + PlayerStats.workHarderCost;
+        workHarderMaxLvlTxt.text = "Nivel: " + PlayerStats.workHarderLevel + "/" + PlayerStats.maxLvl;
+        effLvlCostTxt.text = "Coste: $" + PlayerStats.efficiencyCost;
+        effMaxLvlTxt.text = "Nivel: " + PlayerStats.efficiencyLvl + "/" + PlayerStats.maxLvl;
     }
 
     public void ContinueStory(){
         if(PlayerStats.storyProgress == 0){
-            PlayerStats.storyProgress = -1;
+            PlayerStats.storyProgress = 1;
             main.SetActive(true);
             story.SetActive(false);
         }
     }
-
     
     public void MakeMoney(){
-        PlayerStats.money += 1;
-        moneyCounter.text = "$" + PlayerStats.money.ToString();
+        PlayerStats.money += (int)((1 + PlayerStats.workHarderLevel) * PlayerStats.efficiencyPower);
+        moneyCounter.text = "$" + PlayerStats.money;
+    }
+
+    public void LevelWork(){
+        if(PlayerStats.money >= PlayerStats.workHarderCost && PlayerStats.workHarderLevel < PlayerStats.maxLvl){
+            PlayerStats.money -= PlayerStats.workHarderCost;
+            PlayerStats.workHarderLevel++;
+            PlayerStats.workHarderCost += 5 + PlayerStats.workHarderLevel;
+            workHarderLvlCostTxt.text = "Coste: $" + PlayerStats.workHarderCost;
+            workHarderMaxLvlTxt.text = "Nivel: " + PlayerStats.workHarderLevel + "/" + PlayerStats.maxLvl;
+            moneyCounter.text = "$" + PlayerStats.money;
+        }
+    }
+
+    public void LevelEfficiency(){
+        if(PlayerStats.money >= PlayerStats.efficiencyCost && PlayerStats.efficiencyLvl < PlayerStats.maxLvl){
+            PlayerStats.money -= PlayerStats.efficiencyCost;
+            PlayerStats.efficiencyLvl++;
+            PlayerStats.efficiencyPower += 0.1;
+            PlayerStats.efficiencyCost += 100 + PlayerStats.efficiencyLvl;
+            effLvlCostTxt.text = "Coste: $" + PlayerStats.efficiencyCost;
+            effMaxLvlTxt.text = "Nivel: " + PlayerStats.efficiencyLvl + "/" + PlayerStats.maxLvl;
+            moneyCounter.text = "$" + PlayerStats.money;
+        }
     }
 
     // Update is called once per frame
--- Scripts/Game/PlayerStats.cs
+++ Scripts/Game/PlayerStats.cs
@@ -4,4 +4,11 @@ public static class PlayerStats
 {
     public static int money;
     public static int storyProgress = 0;
+    public static int workHarderLevel = 0;
+    public static int workHarderCost = 5;
+    public static int maxLvl = 5;
+    public static int efficiencyLvl = 0;
+    public static double efficiencyPower = 1;
+    public static int efficiencyCost = 100;
+
 }
